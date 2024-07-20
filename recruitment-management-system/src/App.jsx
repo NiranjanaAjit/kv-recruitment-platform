b85b62ec91f79d73bb6946de0fc2e656959e9d2a
Shanidh <shanidhkhanmepathur@gmail.com>
2024-07-20 07:52:53 +0530

    Added Home Layout Component: Added Aside Component. Home Layout not Complete

---
+++ recruitment-management-system/src/App.jsx
@@ -0,0 +1,13 @@
+import HomeLayout from "./Pages/HomeLayout/HomeLayout"
+import "../src/App.scss"
+
+function App() {
+
+  return (
+    <>
+    <HomeLayout></HomeLayout>
+    </>
+  )
+}
+
+export default App
---
+++ recruitment-management-system/src/Pages/HomeLayout/HomeLayout.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../Components/Sidebar/Sidebar"
+import "../HomeLayout/HomeLayout.scss"
+const HomeLayout = ()=>{
+    return (
+        <div className="homeLayoutContainer">
+            <Sidebar></Sidebar>
+        </div>
+    )
+}
+
+export default HomeLayout;
