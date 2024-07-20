a828c8935cbbca5fe7d573fae3b25b5457d016cc
AdarshS64 <adarshmeant4gr8@gmail.com>
2024-07-20 12:43:55 +0530

    feat: Added route in App.jsx

--- recruitment-management-system/src/App.jsx
+++ recruitment-management-system/src/App.jsx
@@ -1,13 +1,29 @@
-import HomeLayout from "./Pages/HomeLayout/HomeLayout"
-import "../src/App.scss"
+import HomeLayout from "./Pages/HomeLayout/HomeLayout";
+import "../src/App.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      //ADDITION : Login Page and assign
+      // path: "/",
+      // element: <Login />,
+      // errorElement: <ErrorPage />,
+    },
+
+    {
+      path: "/", //CHANGES:  Path needs to be Changed
+      element: <HomeLayout />,
+
+      // ADDITION : children: Needs to be added
+    },
+  ]);
 
   return (
     <>
-    <HomeLayout></HomeLayout>
+      <RouterProvider router={route}></RouterProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
