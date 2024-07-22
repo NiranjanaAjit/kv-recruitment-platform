ca033d36ed89a0ef00330bc06dc3fb550537fa62
Shanidh <shanidhkhanmepathur@gmail.com>
2024-07-22 14:59:33 +0530

    Job card, employee details changes

---
+++ recruitment-management-system/src/components/Job Card/JobCard.jsx
@@ -0,0 +1,49 @@
+import "../Job Card/JobCard.scss";
+
+const JobCard = (props) => {
+	const {
+		id,
+		position,
+		location,
+		created_at,
+		experience,
+		noOfOpening,
+		active,
+	} = props;
+	
+	return (
+		<div className={`job-card ${!active && " border--color"}`}>
+			<div className="job-card-upper-details">
+				<p className="job-card-position-id">Position ID: {id}</p>
+				<p className="job-card-title">{position}</p>
+				<p className="job-card-location">{location}</p>
+
+				{/* <p className="job-card-vacancies">Candidates : </p> */}
+			</div>
+			<div className="job-card-vacancies-container">
+				<div className="job-card-vacancies-numbers total">
+					<p className="job-card-vacancies-numbers-title ">
+						VACANCIES
+					</p>
+					<p className="job-card-vacancies-numbers-amount-vacancy">
+						{noOfOpening}
+					</p>
+				</div>
+				<div className="job-card-vacancies-numbers experience">
+					<p className="job-card-vacancies-numbers-title">
+						EXPERIENCE
+					</p>
+					<p className="job-card-vacancies-numbers-amount-location">
+						{experience}
+					</p>
+				</div>
+			</div>
+			<button className="job-card-refer">Refer a friend</button>
+			<div className="job-card-bottom">
+				<p className="job-card-posted">Posted : {created_at} </p>
+			</div>
+		</div>
+	);
+};
+
+export default JobCard;
