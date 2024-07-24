9e975c137810ef6e1f71cf1a4bf549cc9501e50c
NiranjanaAJit <niranjana3121@gmail.com>
2024-07-25 00:26:40 +0530

    style: not found page

--- recruitment-management-system/src/pages/NotFound/NotFound.jsx
+++ recruitment-management-system/src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Cow from "../../assets/notFoundCow.jpg";
+import "./NotFound.scss";
 const NotFound = () => {
-  return <div>Page not found</div>;
+  return (
+    <div className="full-page">
+      <h2>OOPS ! <br/>Page not found .</h2>
+      <img src={Cow} alt="not found 404 error" />
+    </div>
+  );
 };
 
 export default NotFound;
