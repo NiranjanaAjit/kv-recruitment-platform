c4df1db0bb1f63f12865db72c86e3caa3ac7080c
AdarshS64 <adarshmeant4gr8@gmail.com>
2024-07-24 18:47:11 +0530

    Integrating Employee Api's

--- recruitment-management-system/src/api/employeeApi.js
+++ recruitment-management-system/src/api/employeeApi.js
@@ -4,8 +4,44 @@ const employeeApi = apiWithTag.injectEndpoints({
   endpoints: (builder) => ({
     getEmployeeList: builder.query({
       query: () => "/employee",
+      providesTags: ["EMPLOYEE_LIST"],
+    }),
+    getEmployeeListById: builder.query({
+      query: ({ id }) => ({
+        url: `/employee/${id}`,
+        method: "GET",
+      }),
+    }),
+    editEmployeeList: builder.mutation({
+      query: ({ id: id, payload: payload }) => ({
+        url: `/employee/${id}`,
+        method: "PUT",
+        body: payload,
+      }),
+      invalidatesTags: ["EMPLOYEE_LIST"],
+    }),
+    postEmployeeList: builder.mutation({
+      query: (payload) => ({
+        url: `/employee`,
+        method: "POST",
+        body: payload,
+      }),
+      invalidatesTags: ["EMPLOYEE_LIST"],
+    }),
+    deleteEmployeeListById: builder.mutation({
+      query: (payload) => ({
+        url: `/employee/${payload.id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["EMPLOYEE_LIST"],
     }),
   }),
 });
 
-export const { useGetEmployeeListQuery } = employeeApi;
+export const {
+  useGetEmployeeListQuery,
+  useEditEmployeeListMutation,
+  useGetEmployeeListByIdQuery,
+  usePostEmployeeListMutation,
+  useDeleteEmployeeListByIdMutation,
+} = employeeApi;
