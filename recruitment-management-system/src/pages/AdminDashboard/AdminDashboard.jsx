d432a97ebe914f6a77f07de2cd928ffe2a0dbc53
AdarshS64 <adarshmeant4gr8@gmail.com>
2024-07-24 20:40:36 +0530

    Completed Create Job and Fixed Close Job integration issues

--- recruitment-management-system/src/pages/AdminDashboard/AdminDashboard.jsx
+++ recruitment-management-system/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -20,7 +20,11 @@ const AdminDashboard = () => {
   }));
   return (
     <>
-      <ContentHeader title={`${jobs?.length} Active Jobs`} />
+      <ContentHeader title={`${jobs?.length} Active Jobs`}>
+        <button className="job-card-refer" onClick={(e) => onRefer(e, id)}>
+          Create Job
+        </button>
+      </ContentHeader>
       <JobsList jobs={jobs} />
     </>
   );
