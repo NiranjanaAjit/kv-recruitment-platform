7b4fcb83ef9c2027caa3589cd6f8e0ddf25a0fe5
AdarshS64 <adarshmeant4gr8@gmail.com>
2024-07-24 20:40:36 +0530

    Completed Create Job and Fixed Close Job integration issues

--- recruitment-management-system/src/api/jobApi.js
+++ recruitment-management-system/src/api/jobApi.js
@@ -15,6 +15,21 @@ const jobApi = apiWithTag.injectEndpoints({
         method: "DELETE",
       }),
     }),
+    createJobDetails: builder.mutation({
+      query: (payload) => ({
+        url: `/jobs`,
+        method: "POST",
+        body: payload,
+      }),
+      invalidatesTags: ["JOB_LIST"],
+    }),
+    putJobDetails: builder.mutation({
+      query: ({id,payload}) => ({
+        url: `/jobs/${id}`,
+        method: "PUT",
+        body:payload
+      }),
+    }),
   }),
 });
 
@@ -22,4 +37,6 @@ export const {
   useGetJobListQuery,
   useGetJobDetailsQuery,
   useDeleteJobDetailsMutation,
+  useCreateJobDetailsMutation,
+  usePutJobDetailsMutation
 } = jobApi;
